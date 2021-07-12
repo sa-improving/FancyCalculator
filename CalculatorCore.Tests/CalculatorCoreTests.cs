3e9b2d431803a6133e04f7589af1863129f7c051
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-07-12 16:23:00 -0500

    Refactored tests

--- CalculatorCore.Tests/CalculatorCoreTests.cs
+++ CalculatorCore.Tests/CalculatorCoreTests.cs
@@ -9,21 +9,22 @@ namespace CalculatorCore.Tests
         [TestMethod]
         public void AddTwoNumbers()
         {
-            
             var result = calc.Evaluate("6 + 8").Result;
             Assert.AreEqual(14, result);
         }
 
+        [TestMethod]
         public void IncorrectFirstValue()
         {
             var result = calc.Evaluate("A + 2").ErrorMessage;
-            Assert.AreEqual("The first value, A, is not a valid number", result);
+            Assert.AreEqual("The first value, 'A', is not a valid number", result);
         }
 
+        [TestMethod]
         public void IncorrectSecondValue()
         {
             var result = calc.Evaluate("2 + B").ErrorMessage;
-            Assert.AreEqual("The secon value, B, is not a valid number", result);
+            Assert.AreEqual("The second value, 'B', is not a valid number", result);
         }
     }
 }
