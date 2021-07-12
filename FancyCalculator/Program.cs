eeddf35762dffea510e7647e981f7f77163b2a78
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-07-12 10:23:05 -0500

    Intitial Commit

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -8,23 +8,29 @@ namespace FancyCalculator
         {
             Console.WriteLine("A Console Calculator");
 
-            Console.WriteLine("Enter a Number");
+            Console.WriteLine("Enter what you would like to see added.");
+            var expressionInput = Console.ReadLine().Trim();
+            var expressionNumbers = expressionInput.Split(" + ");
+
+
+            //Console.WriteLine("Enter a Number");
 
             var firstInput = Console.ReadLine().Trim();
             double firstNumber;
-            if (!Double.TryParse(firstInput, out firstNumber))
+            if (!Double.TryParse(expressionNumbers[0], out firstNumber))
             {
                 Console.WriteLine("The first value, {0}, was not a vaild number", firstNumber);
                 return;
             }
 
-            Console.WriteLine("Enter a second number, and I will add it to the first.");
+            //Console.WriteLine("Enter a second number, and I will add it to the first.");
 
             var secondInput = Console.ReadLine().Trim();
             double secondNumber;
-            if (!Double.TryParse(secondInput, out secondNumber))
+            if (!Double.TryParse(expressionNumbers[1], out secondNumber))
             {
-                Console.WriteLine("The second value, {0}, was not a vaild number", secondInput);
+                Console.WriteLine("The second value, {0}, was not a vaild number", secondInput
+                    );
                 return;
             }
 
